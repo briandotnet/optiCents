3ea6dfc120b45c438d7394c688901720c42b2e8d
André Dion <andredion@gmail.com>
2011-07-19 16:20:54 -0300

    WIP layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,67 +1,66 @@
 class HomeController < ApplicationController
-  # responde to GET
-  def index
-      if params[:oauth_token] then # handles call back from dropbox
-        logger.debug "handle call back"
-        dropbox_session = Dropbox::Session.deserialize(session[:dropbox_session])
-        dropbox_session.authorize(params)
-        session[:dropbox_session] = dropbox_session.serialize # re-serialize the authenticated dropbox_session 
-      else
-        if session[:dropbox_session].nil? then # user landed with no previous dropbox_session
-          logger.debug "no previous session available"
-          app_key = "tcu08l85y39ybxd" #DROPBOX_CONFIG['app_key']
-          app_secret = "3daxhdk2813pcng" #DROPBOX_CONFIG['app_secret']
-          # create a new dropbox_session and save in session variable
-          dropbox_session = Dropbox::Session.new(app_key, app_secret)
-          session[:dropbox_session] = dropbox_session.serialize
 
-          redirect_to dropbox_session.authorize_url(:oauth_callback => url_for(:controller => 'home', :action => 'index'))
-        else # user landed with previous dropbox_session
-          logger.debug "use previous session"
-          dropbox_session = Dropbox::Session.deserialize(session[:dropbox_session])
-          if !dropbox_session.authorized? then 
-            # dropbox_session is not authorized
-            logger.debug "redirect to dropbox oauth page"
-            redirect_to dropbox_session.authorize_url(:oauth_callback => url_for(:controller => 'home', :action => 'index'))
-          end
-        end
-      end
-    end
-  
-    # respond to json request for photo paths
-    def photos
-      if session[:dropbox_session].nil? then
-        logger.debug 'no previous session available'
-        return redirect_to(:controller => 'home', :action => 'index')
-      else
-        logger.debug 'previous session found'
+  def index
+    if params[:oauth_token] then # handles call back from dropbox
+      logger.debug "handle call back"
+      dropbox_session = Dropbox::Session.deserialize(session[:dropbox_session])
+      dropbox_session.authorize(params)
+      session[:dropbox_session] = dropbox_session.serialize # re-serialize the authenticated dropbox_session 
+    else
+      if session[:dropbox_session].nil? then # user landed with no previous dropbox_session
+        logger.debug "no previous session available"
+        app_key = "tcu08l85y39ybxd" #DROPBOX_CONFIG['app_key']
+        app_secret = "3daxhdk2813pcng" #DROPBOX_CONFIG['app_secret']
+        # create a new dropbox_session and save in session variable
+        dropbox_session = Dropbox::Session.new(app_key, app_secret)
+        session[:dropbox_session] = dropbox_session.serialize
+
+        redirect_to dropbox_session.authorize_url(:oauth_callback => url_for(:controller => 'home', :action => 'index'))
+      else # user landed with previous dropbox_session
+        logger.debug "use previous session"
         dropbox_session = Dropbox::Session.deserialize(session[:dropbox_session])
-        dropbox_session.mode = :dropbox
-              
-        if dropbox_session.authorized? then
-          
-          photos = Array.new
-          
-          begin
-            metadata = dropbox_session.list('/public/%s' % params[:item_number])
-            # photoStruct = Struct.new(:path, :width, :height)
-            metadata.each do |photo|
-              logger.debug photo.inspect
-              path = photo['path']
-              path['/Public'] = 'http://dl.dropbox.com/u/8319978'
-              # width = photo['width']
-              # height = photo['height']
-              photos.push path
-            end
-          rescue
-            # do nothing for now... need to handle dropbox error
-          end
+        
+        if !dropbox_session.authorized? then 
+          # dropbox_session is not authorized
+          logger.debug "redirect to dropbox oauth page"
+          redirect_to dropbox_session.authorize_url(:oauth_callback => url_for(:controller => 'home', :action => 'index'))
         end
       end
-      
-      respond_to do |format|
-        format.json { render :json => photos.to_json }
+    end
+  end
+
+  # respond to json request for photo paths
+  def photos
+    if session[:dropbox_session].nil? then
+      logger.debug 'no previous session available'
+      return redirect_to(:controller => 'home', :action => 'index')
+    else
+      logger.debug 'previous session found'
+      dropbox_session = Dropbox::Session.deserialize(session[:dropbox_session])
+      dropbox_session.mode = :dropbox
+              
+      if dropbox_session.authorized? then
+        photos = Array.new
+
+        begin
+          metadata = dropbox_session.list('/public/%s' % params[:n])
+          # photoStruct = Struct.new(:path, :width, :height)
+          metadata.each do |photo|
+            logger.debug photo.inspect
+            path = photo['path']
+            path['/Public'] = 'http://dl.dropbox.com/u/8319978'
+            # width = photo['width']
+            # height = photo['height']
+            photos.push path
+          end
+        rescue
+          # do nothing for now... need to handle dropbox error
+        end
       end
     end
-    
+
+    respond_to do |format|
+      format.json { render :json => photos.to_json }
+    end
+  end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,56 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+(function( $ ) {
+  
+  // retrieves and injects into the DOM a collection of photos that belong to the given ID
+  var getPhotos = function( id ) {
+    var container = $('#container'),
+      msg = $('#messaging');
+
+    // always clear old messages
+    msg.removeClass();
+
+    $.getJSON('photos.json?n=' + id, function( data ) {
+
+      // no results :(
+      if( !data.length ) {
+        msg.attr('class', 'error').html('No photos found for item <b>#' + id + '</b>!');
+      }
+
+      // we have data!
+      else {
+        var items = [],
+          gallery = $('<div/>', { id: 'gallery' });
+
+        $.each(data, function( key, val ) {
+          items.push('<li><img src="' + val + '" width="320" height="320"></li>');
+        });
+
+        $('<ul/>', { html: items.join('') }).appendTo(gallery);
+        container.empty().append(gallery);
+
+        gallery.slideViewerPro({
+          thumbs: 5, 
+          thumbsPercentReduction: 15,
+          galBorderWidth: 0,
+          thumbsTopMargin: 10,
+          thumbsRightMargin: 10,
+          thumbsBorderWidth: 5,
+          thumbsActiveBorderColor: "white",
+          thumbsActiveBorderOpacity: 0.5,
+          thumbsBorderOpacity: 0,
+          buttonsTextColor: "#707070",
+          leftButtonInner: "<",
+          rightButtonInner: ">",
+          autoslide: false
+        });
+      }
+    });
+  };
+
+  // catch all form submissions and proxy them through an AJAX request
+  $(document).ready(function() {
+    $('#itemLookup').submit(function( e ) {
+      if( e ) { e.preventDefault(); }
+      getPhotos($('#itemNumber').val().trim());
+    });
+  });
+})(jQuery);

a2b054a720fb83dfc5d655421669b678a17d50dc
Kathryn Duffy <kathrynduffy@Kathryn-Duffys-MacBook-Pro.local>
2011-09-12 09:58:43 -0300

    Changes requested by Fred: additional fields for done, product materials, and item comments.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,4 @@
+
 class HomeController < ApplicationController
 
   # respond to index request for all items list
@@ -10,6 +11,10 @@ class HomeController < ApplicationController
   
   # respond to item request
   def item
+    if params[:id] == nil and params[:itemNumber] != nil then
+      params[:id] = params[:itemNumber]
+    end
+    
     if params[:oauth_token] then # handles call back from dropbox
       logger.debug "handle call back"
       dropbox_session = Dropbox::Session.deserialize(session[:dropbox_session])
@@ -35,11 +40,37 @@ class HomeController < ApplicationController
       else # user landed with previous dropbox_session
         logger.debug "use previous session"
         dropbox_session = Dropbox::Session.deserialize(session[:dropbox_session])
+        dropbox_session.mode = :dropbox
         
         if !dropbox_session.authorized? then 
           # dropbox_session is not authorized
           logger.debug "redirect to dropbox oauth page"
           redirect_to dropbox_session.authorize_url(:oauth_callback => url_for(:controller => 'home', :action => 'item'))
+        else 
+          begin
+            file_contents = dropbox_session.download('/public/%s/details.xml' % params[:id])
+            logger.debug "DETAILS.XML: %s" % file_contents
+            str = "%s" % file_contents
+            str = str.gsub(/\r/, "")
+            str = str.gsub(/\n/, "")
+          
+            str.scan (/\<key\>comments\<\/key\>\s+\<string\>(.*?)\<\/string\>/) {
+              params[:comments] = "#{$1}"
+            }
+            str.scan (/\<key\>productMaterial\<\/key\>\s+\<string\>(.*?)\<\/string\>/) {
+              params[:product_materials] = "#{$1}"
+            }
+            str.scan (/\<key\>done\<\/key\>\s+\<string\>(.*?)\<\/string\>/) {
+              params[:done] = false
+              if "#{$1}" == "true" then
+                params[:done] = true
+              end
+            }
+          rescue
+            params[:comments] = ""
+            params[:product_materials] = ""
+            params[:done] = false
+          end
         end
       end
     end
@@ -49,7 +80,32 @@ class HomeController < ApplicationController
     #render item.html.erb
   end
 
+  # respond to request to get item details
+  def details
+    if session[:dropbox_session].nil? then
+      logger.debug 'no previous session available'
+      return redirect_to(:controller => 'home', :action => 'item')
+    else
+      logger.debug 'previous session found'
+      dropbox_session = Dropbox::Session.deserialize(session[:dropbox_session])
+      dropbox_session.mode = :dropbox
+      
+      if dropbox_session.authorized? then        
+        if params[:n].nil? then
+          logger.debug 'Requesting details with no parameters!!!!!'
+        else
+          details = dropbox_session.download('/public/%s/details.xml' % params[:n])
+          logger.debug "DETAILS.XML: %s" % details
+          
+        end
+        # this line throw error on heroku but not in localhost
+        # logger.error $!, $!.backtrace
+      end
+    end
+  end
+  
 
+  
   # respond to json request for photo paths
   def photos
     if session[:dropbox_session].nil? then
@@ -64,15 +120,22 @@ class HomeController < ApplicationController
         photos = Array.new
 
         begin
-          metadata = dropbox_session.list('/public/%s' % params[:n])
-          # photoStruct = Struct.new(:path, :width, :height)
-          metadata.each do |photo|
-            # logger.debug photo.inspect
-            path = photo['path']
-            path['/Public'] = 'http://dl.dropbox.com/u/8319978'
-            # width = photo['width']
-            # height = photo['height']
-            photos.push path
+            if params[:n].nil? then
+                logger.debug 'Requesting Photos with no parameters!!!!!'
+            else
+                metadata = dropbox_session.list('/public/%s' % params[:n])
+                # photoStruct = Struct.new(:path, :width, :height)
+                metadata.each do |photo|
+                  #logger.debug photo.inspect
+                if photo['mime_type'] == "image/png" then
+                  path = photo['path']
+                  path['/Public'] = 'http://dl.dropbox.com/u/8319978'
+                  # width = photo['width']
+                  # height = photo['height']
+                  logger.debug "path is now: %s" % path
+                  photos.push path
+                end
+            end
           end
         rescue
           # this line throw error on heroku but not in localhost
@@ -83,6 +146,65 @@ class HomeController < ApplicationController
 
     respond_to do |format|
       format.json { render :json => photos.to_json }
+    end
+  end
+  
+  def save 
+    if session[:dropbox_session].nil? then
+      logger.debug 'no previous session available'
+      return redirect_to(:controller => 'home', :action => 'item')
+      else
+      logger.debug 'previous session found'
+      dropbox_session = Dropbox::Session.deserialize(session[:dropbox_session])
+      dropbox_session.mode = :dropbox
+      
+      if dropbox_session.authorized? then
+        if params[:id] then
+          begin
+            @file = Tempfile.new('%s' % params[:id])
+            begin
+              done = false
+              if params[:done] == "yes" then
+                done = true
+              end
+              @contents = <<-EOF
+              <?xml version="1.0" encoding="UTF-8"?>
+              <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+              <plist version="1.0">
+              <dict>
+              <key>comments</key>
+              <string>#{params[:comments]}</string>
+              
+              <key>productMaterial</key>
+              <string>#{params[:product_materials]}</string>
+              
+              <key>done</key>
+              <string>#{done}</string>
+              </dict>
+              </plist>
+              EOF
+              logger.debug "CONTENTS OF NEW DETAILS.XML: %s" % @contents
+              @file.print @contents
+              @file.flush
+              
+              dropbox_session.delete('/public/%s/details.xml' % params[:id])
+              logger.debug "TEMPORARY FILE PATH: %s" % @file.path
+              dropbox_session.upload("%s" % @file.path, '/public/%s/' % params[:id])
+              filename = "%s" % @file.path
+              filename = filename.gsub(/^.*\//,"")
+              logger.debug "TEMPORARY FILE NAME: %s" % filename
+              dropbox_session.rename('/public/%s/%s' % [params[:id], filename], 'details.xml')
+            ensure
+              @file.close
+              @file.unlink   # deletes the temp file
+            end
+            
+            redirect_to url_for(:controller=>'home', :action => 'index')
+          rescue
+            logger.error $!, $!.backtrace
+          end
+        end
+      end
     end
   end
   
@@ -124,16 +246,21 @@ class HomeController < ApplicationController
 
         begin
           metadata = dropbox_session.list('/public/')
-          Struct.new("Item", :number, :date)
+            Struct.new("Item", :done, :number, :date)
           metadata.each do |item|
             # logger.debug item.inspect
-            logger.debug item
-
+            logger.debug item            
+            
             number = item['path']
             number['/Public/'] = ''# url_for(:controller => 'home', :action => 'index')
             date = item['modified']
+            
+            #xml = File.read('http://dl.dropbox.com/u/8319978/%s' % number)
+            logger.debug 'XML DETAIL FILE LOCATION: http://dl.dropbox.com/u/8319978/%s/details.xml' % number 
+            #logger.debug 'READ XML DETAIL FILE: %s' % xml
+            
             if(number.casecmp("archive") != 0 ) then
-              item_numbers.push Struct::Item.new(number, date.strftime("%b %d, %Y"))
+              item_numbers.push Struct::Item.new(false, number, date.strftime("%b %d, %Y"))
             end
           end
             logger.debug item_numbers.inspect

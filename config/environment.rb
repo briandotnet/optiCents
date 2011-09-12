733f835c0d78e0b3f9aded04f82ba81b93e6180e
Kathryn Duffy <kathrynduffy@Kathryn-Duffys-MacBook-Pro.local>
2011-09-12 08:43:53 -0300

    Rolling back modifications since we get an app error

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-require 'Plist'
-
 # Initialize the rails application
 Snap::Application.initialize!
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,6 @@ Snap::Application.routes.draw do
   get "home/index"
   get "home/photos"
   get "home/delete"
-  get "home/details"
-  get "home/save"
   
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -56,7 +54,7 @@ Snap::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "home#index"
+  root :to => "home#item"
   
   # See how all your routes lay out with "rake routes"
 

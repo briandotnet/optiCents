bdc1303238e9e7d41d8b3bd0c1ea22649fa4e356
Brian Ge <gebing@gmail.com>
2012-02-29 00:24:34 -0400

    add store chain to form, show and index view

--- app/models/store.rb
+++ app/models/store.rb
@@ -3,4 +3,7 @@ class Store < ActiveRecord::Base
   acts_as_mappable  :defanlt_units => :kms,
                     :lat_column_name => :lat,
                     :lng_column_name => :lng
+  def store_chain_name
+    return self.store_chain.name
+  end
 end
